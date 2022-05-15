5681a7507cc524080a5a250219fcca5f3a35b535
Denyse <mdenyse15@gmail.com>
2022-05-15 14:55:10 +0200

    feat: splash screen and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./screens/**/*.{js,jsx,ts,tsx}','./components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        primary: ['space-mono', 'sanserif'],
+      },
+      colors:{
+        orange: "#F7941D"
+      }
+    },
+   
   },
   plugins: [],
   corePlugins: require('tailwind-rn/unsupported-core-plugins'),
